255a36813bf32bae98e7469cf8f89dd04c901148
Ethan Q Zhong <ethan.q.zhong@cn.pwc.com>
2022-02-15 13:16:30 +0800

    [Add]gh-graph first commit

--- lib/style/size.dart
+++
@@ -1,7 +0,0 @@
-class SysSize {
-  static const double huge = 22;
-  static const double big = 16;
-  static const double normal = 14;
-  static const double small = 12;
-  static const double tiny = 10;
-}
